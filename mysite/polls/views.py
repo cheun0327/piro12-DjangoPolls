b11aaf059edc2165bd5a87321bbd227846fe8885
LeoChaeeeun <cheun0327@gmail.com>
2020-01-23 00:38:25 +0900

    part 4
    minimal form작성하기(투표)
    generic view사용하기(CBV)

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,35 +1,72 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.http import Http404
-from polls.models import Question
+from django.urls import reverse
+from django.views import generic
 
+from polls.models import Question, Choice
 
+''' # FBV
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index")
 
+
 def detail(request, question_id):
     try:
-        question=Question.objects.get(pk=question_id)
+        question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
     # question=get_object_or_404(Question, pk=question_id)로 대체가능
 
-    return render(request, 'polls/detail.html', {'question':question})
+    return render(request, 'polls/detail.html', {'question': question})
+
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+'''
+
+# CBV
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request,
+                      'polls/detail.html', {
+                          'question': question,
+                          'error_message': "You didn't select a choice.",
+                      })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
 
 ## 여기부분 잘 이해안댐...
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template('polls/index.html')
-    context={   # 템플릿에서 쓰이는 변수명과 파이썬 객체를 연결하는 딕셔너리 값
-        'latest_question_list' : latest_question_list,
+    context = {  # 템플릿에서 쓰이는 변수명과 파이썬 객체를 연결하는 딕셔너리 값
+        'latest_question_list': latest_question_list,
     }
-    #return HttpResponse(template.render(context, request))
+    # return HttpResponse(template.render(context, request))
     return render(request, 'polls/index.html', context)
